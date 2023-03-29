abaaf5335d23d2fbe1782566dc6f54a15fed5291
Ali <alexander.antoshuk@gmail.com>
2023-03-29 16:42:58 +0300

    fix: forget to replace $USER in service files

--- install-server.sh
+++ install-server.sh
@@ -3,9 +3,9 @@ NAME=fsevents-sync-server
 
 cp -f ./.$NAME.json ~/.$NAME.json
 sudo cp -f ./scripts/$NAME.py /usr/local/bin/$NAME.py
-sudo sed -i "s/\$USER/$USER/g" /usr/local/bin/$NAME.py
 sudo systemctl disable $NAME.service
 sudo cp -f ./$NAME.service /etc/systemd/system/$NAME.service
+sudo sed -i "s/\$USER/$USER/g" /etc/systemd/system/$NAME.service
 sudo systemctl daemon-reload
 sudo systemctl enable $NAME.service
 sudo systemctl start $NAME.service
