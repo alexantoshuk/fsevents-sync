637f601bff51886607784fdc820ffccd35524e96
Ali <alexander.antoshuk@gmail.com>
2023-03-30 12:29:14 +0300

    fix: error  with var replace

--- install-client.sh
+++ install-client.sh
@@ -6,7 +6,7 @@ cp -f ./.$NAME.json ~/.$NAME.json
 sudo cp -f ./scripts/$NAME.py /usr/local/bin/$NAME.py
 sudo systemctl disable $NAME.service
 sudo cp -f ./$NAME.service /etc/systemd/system/$NAME.service
-sudo sed -i "s/\$USER/$USER/g; s/\$FSEVENTSDIR/$FSEVENTSDIR/g" /etc/systemd/system/$NAME.service
+sudo sed -i "s|\$USER|$USER|g;s|\$FSEVENTSDIR|$FSEVENTSDIR|g" /etc/systemd/system/$NAME.service
 sudo systemctl daemon-reload
 sudo systemctl enable $NAME.service
 sudo systemctl start $NAME.service
