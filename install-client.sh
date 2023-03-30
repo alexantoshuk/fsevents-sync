1eb76527ffb0fbfe960148cbcdd6cca566ec4c6d
Ali <alexander.antoshuk@gmail.com>
2023-03-30 12:14:35 +0300

    feat: set install path in one place

--- install-client.sh
+++ install-client.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 NAME=fsevents-sync-client
+FSEVENTSDIR = /mnt/data/tk/scripts/fsevents
 
 cp -f ./.$NAME.json ~/.$NAME.json
 sudo cp -f ./scripts/$NAME.py /usr/local/bin/$NAME.py
 sudo systemctl disable $NAME.service
 sudo cp -f ./$NAME.service /etc/systemd/system/$NAME.service
-sudo sed -i "s/\$USER/$USER/g" /etc/systemd/system/$NAME.service
+sudo sed -i "s/\$USER/$USER/g; s/\$FSEVENTSDIR/$FSEVENTSDIR/g" /etc/systemd/system/$NAME.service
 sudo systemctl daemon-reload
 sudo systemctl enable $NAME.service
 sudo systemctl start $NAME.service
